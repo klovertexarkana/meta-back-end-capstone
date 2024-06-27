6b8ab792789d981fd1061175bfd55e26a20e2606
klovertexarkana <jsatterfield81@gmail.com>
2024-06-27 15:10:00 -0500

    set up models and mysql connection

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
 
 # Create your models here.
+
+class Booking(models.Model):
+    id = models.BigIntegerField(primary_key=True)
+    name = models.CharField(max_length=255)
+    no_of_guests = models.IntegerField(validators=[MaxValueValidator(999999)])
+    booking_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.name
+
+class Menu(models.Model):
+   id  = models.SmallIntegerField(primary_key=True)
+   title = models.CharField(max_length=255)
+   price = models.DecimalField(max_digits=10, decimal_places=2)
+   inventory = models.SmallIntegerField()
+
+   def __str__(self):
+      return self.title
